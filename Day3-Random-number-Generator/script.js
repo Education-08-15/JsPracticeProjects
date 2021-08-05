a4088005624068be80042e6129cab033adb76816
Shikshya Gautam <nanu.gautam2016@gmail.com>
2021-08-04 21:26:11 -0400

    random number generator between 1-10

--- Day3-Random-number-Generator/script.js
+++ Day3-Random-number-Generator/script.js
@@ -1,1 +1,8 @@
-console.log('hello');
+const num = document.querySelector('.num');
+const btn = document.querySelector('.generate');
+
+const generateNumber = () => {
+  const randomNumber = Math.floor(Math.random() * 10 + 1);
+  num.innerHTML = `${randomNumber}`;
+};
+btn.addEventListener('click', generateNumber);
